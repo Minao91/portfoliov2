a0f7a86a5a318ce8148939ed68371da43ee292cb
Minao <antoine1.geoffroy@epitech.zu>
2021-12-26 17:01:45 +0100

    [01|ADD] Start project react

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/home/home';
+import Error from './components/error/404';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error/>} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/home/home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            Home
+            <a href="#" target="_blank">test</a>
+        </>
+    );
+  }
+  
+  export default Home;
